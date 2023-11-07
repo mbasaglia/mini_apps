001463324fa7b1c858a2987d68624b73e97c1e3f
Mattia Basaglia <glax@dragon.best>
2023-11-07 10:30:33 +0100

    Script to vew app settings

---
+++ src/app-settings.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import sys
+import json
+import argparse
+
+from mini_apps.settings import Settings
+
+
+parser = argparse.ArgumentParser(description="Show settings for an app")
+parser.add_argument(
+    "app",
+    help="App name"
+)
+parser.add_argument(
+    "--globals", "-g",
+    action="store_true",
+    help="Include global settings",
+)
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    global_settings = Settings.load_global()
+    app_settings = global_settings.apps[args.app].settings
+
+    values = {}
+    if args.globals:
+        value = global_settings.data
+    values.update(app_settings._data)
+
+    print(json.dumps(values, indent=4))
